fe3dfecd82916546e7d3dd76cd98fa3eb22b4b20
Jonas Vittrup Biegel <10519146@ucn.dk>
2025-05-30 08:42:47 +0200

    Created a small test

--- database/src/main.rs
+++ database/src/main.rs
@@ -6,6 +6,17 @@ use std::{
 mod db;
 
 fn main() {
-    let col = db::Column::new(true, db::ColumnContent::Str(BTreeMap::new()));
-    col.insert(rv, v)
+    let row_version: u32 = 1;
+
+    let mut col_string = db::Column::new(true, db::ColumnContent::Str(BTreeMap::new()));
+    let success_string = col_string.insert(row_version, db::Value::Str("hej".to_string()));
+
+    let mut col_int = db::Column::new(false, db::ColumnContent::Int(BTreeMap::new()));
+    let success_int = col_int.insert(row_version, db::Value::Int(2));
+
+    if success_int {
+        println!("gaming");
+    } else {
+        println!("no");
+    }
 }
