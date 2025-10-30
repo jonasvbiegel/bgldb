3f50b5ea1d2a47caa50c5318f449caff61a5ed22
Jonas Vittrup Biegel <10519146@ucn.dk>
2025-10-30 14:46:43 +0100

    created logic for reading pages from file

--- database/btree_file/src/main.rs
+++ database/btree_file/src/main.rs
@@ -1,30 +1,36 @@
 use std::fs::{File, OpenOptions};
-use std::io::{BufWriter, Error, Read, Seek, SeekFrom, Write};
+use std::io::{Error, Read, Seek, SeekFrom, Write};
+
+// BIG ENDIAN BYTES
 
 const PAGESIZE: u64 = 4096;
 type Id = u64;
 
 fn main() -> Result<(), Error> {
-    let mut handler = Handler::new("test")?;
+    let mut handler = FileHandler::new("test")?;
 
-    handler.write_to_header("gaming".as_bytes())?;
+    let page_id = dbg!(handler.new_page()?);
+    handler.write_to_page(page_id, "GUNGA GAMER".as_bytes())?;
 
-    let page_id = handler.new_page()?;
-
-    handler.get_max_id()?;
-    handler.write_to_page(page_id, "hej gamer".as_bytes())?;
-    let read = handler.read_all()?;
+    let read = handler.read_page(page_id);
 
     println!("{read:?}");
+
+    let mut header = Header { elements: 909090 };
+    handler.write_to_header(&header.serialize().unwrap())?;
+
+    let header_read = Header::deserialize(&handler.read_header().unwrap());
+
+    println!("{header_read:?}");
 
     Ok(())
 }
 
-struct Handler {
+struct FileHandler {
     file: File,
 }
 
-impl Handler {
+impl FileHandler {
     fn new(name: &str) -> Result<Self, Error> {
         let mut h = Self {
             file: OpenOptions::new()
@@ -44,9 +50,32 @@ impl Handler {
     }
 
     fn new_page(&mut self) -> Result<Id, Error> {
-        let id = self.file.seek(std::io::SeekFrom::End(0))?;
+        // let id = self.file.seek(std::io::SeekFrom::End(0))?;
+        let id = self.file.seek(SeekFrom::End(0))?;
         self.write(&[0x00; PAGESIZE as usize])?;
         Ok((id / PAGESIZE) - 1)
+    }
+
+    fn write(&mut self, buf: &[u8]) -> Result<(), Error> {
+        self.file.seek(SeekFrom::End(0))?;
+        self.file.write_all(buf)?;
+        Ok(())
+    }
+
+    fn write_to_page(&mut self, id: Id, buf: &[u8]) -> Result<bool, Error> {
+        if id <= self.get_max_id()? {
+            let pos = PAGESIZE + (PAGESIZE * id);
+            self.file.seek(SeekFrom::Start(pos))?;
+            self.file.write_all(buf)?;
+            return Ok(true);
+        }
+        Ok(false)
+    }
+
+    fn write_to_header(&mut self, buf: &[u8]) -> Result<(), Error> {
+        self.file.seek(SeekFrom::Start(0))?;
+        self.file.write_all(buf)?;
+        Ok(())
     }
 
     fn read_all(&mut self) -> Result<Vec<u8>, Error> {
@@ -56,38 +85,54 @@ impl Handler {
         Ok(buf)
     }
 
-    fn write(&mut self, buf: &[u8]) -> Result<(), Error> {
-        self.file.seek(SeekFrom::End(0))?;
-        let mut writer = BufWriter::new(self.file.try_clone().unwrap());
-        writer.write_all(buf)?;
-        Ok(())
+    fn read_page(&mut self, id: Id) -> Result<Vec<u8>, Error> {
+        let pos = PAGESIZE + (PAGESIZE * id);
+        let mut buf: [u8; PAGESIZE as usize] = [0x00; PAGESIZE as usize];
+        self.file.seek(SeekFrom::Start(pos))?;
+        self.file.read_exact(&mut buf)?;
+        Ok(buf.to_vec())
     }
 
-    fn write_to_page(&mut self, id: Id, buf: &[u8]) -> Result<bool, Error> {
-        if dbg!(id <= self.get_max_id()?) {
-            let mut writer = BufWriter::new(self.file.try_clone().unwrap());
-            self.file
-                .seek(SeekFrom::Start(PAGESIZE + (PAGESIZE * id)))?;
-            writer.write_all(buf)?;
-            return Ok(true);
-        }
-        Ok(false)
-    }
-
-    fn write_to_header(&mut self, buf: &[u8]) -> Result<(), Error> {
-        let mut writer = BufWriter::new(self.file.try_clone().unwrap());
-        self.file.seek(SeekFrom::Start(0))?;
-        writer.write_all(buf)?;
-        Ok(())
+    fn read_header(&mut self) -> Result<Vec<u8>, Error> {
+        let mut buf: [u8; PAGESIZE as usize] = [0x00; PAGESIZE as usize];
+        self.file.rewind()?;
+        self.file.read_exact(&mut buf)?;
+        Ok(buf.to_vec())
     }
 
     fn get_max_id(&mut self) -> Result<u64, Error> {
-        Ok((self.file.metadata()?.len() / PAGESIZE) - 2)
-    }
-
-    fn get_header(&self) -> Result<Header, Error> {
-        todo!()
+        let len = self.file.metadata()?.len();
+        Ok((len / PAGESIZE) - 1)
     }
 }
 
-struct Header;
+#[derive(Debug)]
+struct Header {
+    elements: u64,
+}
+
+impl Header {
+    // fn new() -> Self {
+    //     Self { elements: 0 }
+    // }
+
+    fn deserialize(bytes: &[u8]) -> Option<Header> {
+        if bytes.len() != PAGESIZE as usize {
+            return None;
+        }
+
+        let e = u64::from_be_bytes(bytes[0..8].try_into().unwrap());
+
+        Some(Self { elements: e })
+    }
+
+    fn serialize(&mut self) -> Option<Vec<u8>> {
+        let mut b: [u8; PAGESIZE as usize] = [0x00; PAGESIZE as usize];
+
+        for (idx, byte) in self.elements.to_be_bytes().iter().enumerate() {
+            b[idx] = *byte;
+        }
+
+        Some(b.to_vec())
+    }
+}
