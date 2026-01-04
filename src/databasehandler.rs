858538240c0a4e5fac3a2c18b9a6987a9ecb1ebf
Jonas Vittrup Biegel <10519146@ucn.dk>
2026-01-04 20:30:11 +0100

    added error handling to operation function

--- src/databasehandler.rs
+++ src/databasehandler.rs
@@ -1,9 +1,9 @@
-use crate::database::{Database, DatabaseBuilder, KeyTypeSize, page::KeyType};
-use axum::{
-    body::Body,
-    http::{Response, StatusCode},
-};
+pub use crate::database::page::KeyType;
+use crate::database::{Database, DatabaseBuilder, KeyTypeSize};
+use axum::http::{HeaderName, StatusCode, header};
 use std::fs::{File, OpenOptions};
+
+pub type DatabaseResponse = (StatusCode, [(HeaderName, String); 1], String);
 
 pub struct DatabaseHandler {
     db: Database<File>,
@@ -31,21 +31,21 @@ impl DatabaseHandler {
         self.db.get_keytype().unwrap()
     }
 
-    pub fn get_data(&mut self, key: &[u8]) -> Response<Body> {
+    pub fn get_data(&mut self, key: &[u8]) -> DatabaseResponse {
         let found = self.db.get(key);
 
         if let Ok(Some(data)) = found {
-            Response::builder()
-                .status(StatusCode::OK)
-                .header("Content-Type", "application/json")
-                .body(Body::from(data.json()))
-                .unwrap()
+            (
+                StatusCode::OK,
+                [(header::CONTENT_TYPE, "application/json".to_string())],
+                data.json(),
+            )
         } else {
-            Response::builder()
-                .status(StatusCode::NOT_FOUND)
-                .header("Content-Type", "text/plain")
-                .body(Body::from("key not found"))
-                .unwrap()
+            (
+                StatusCode::NOT_FOUND,
+                [(header::CONTENT_TYPE, "text/plain".to_string())],
+                "key not found".to_string(),
+            )
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,81 +1,87 @@
 mod database;
 mod databasehandler;
 
-use databasehandler::DatabaseHandler;
+use crate::databasehandler::KeyType;
+use databasehandler::{DatabaseHandler, DatabaseResponse};
 use serde_json::Value;
 
 use axum::{
     Json, Router,
-    body::Body,
     extract::State,
-    http::{Response, StatusCode},
+    http::{StatusCode, header},
     routing::{get, post},
 };
-use std::sync::Arc;
-use std::{error::Error, sync::Mutex};
+use std::sync::{Arc, Mutex};
 use tokio::net::TcpListener;
-
-use crate::database::page::KeyType;
+use tower_http::cors::{Any, CorsLayer};
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn Error>> {
+async fn main() {
     println!("hosting on localhost:8000");
 
     let database = Arc::new(Mutex::new(DatabaseHandler::new()));
     let address = "localhost:8000".to_string();
 
+    let cors = CorsLayer::new().allow_origin(Any);
+
     let app = Router::new()
         .route("/", post(operation))
         .route("/", get("Hello from bgldb!"))
-        .with_state(database);
+        .with_state(database)
+        .layer(cors);
 
     let listener = TcpListener::bind(address).await.unwrap();
 
     axum::serve(listener, app).await.unwrap();
-
-    Ok(())
 }
 
 async fn operation(
     State(handler): State<Arc<Mutex<DatabaseHandler>>>,
     Json(payload): Json<Value>,
-) -> Response<Body> {
+) -> DatabaseResponse {
     if let Value::String(operation) = &payload["operation"] {
-        match operation.as_str() {
+        match operation.to_uppercase().as_str() {
             "GET" => {
                 if let Ok(mut locked) = handler.lock() {
-                    let keytype = locked.get_keytype();
-                    match (&payload["key"], keytype) {
+                    match (&payload["key"], locked.get_keytype()) {
                         (Value::String(key), KeyType::String) => locked.get_data(key.as_bytes()),
                         (Value::Number(key), KeyType::UInt64) => {
-                            locked.get_data(&key.as_u64().unwrap().to_le_bytes())
+                            if let Some(key) = key.as_u64() {
+                                locked.get_data(&key.to_le_bytes())
+                            } else {
+                                (
+                                    StatusCode::BAD_REQUEST,
+                                    [(header::CONTENT_TYPE, "text/plain".to_string())],
+                                    "expected unsigned integer, found negative number".to_string(),
+                                )
+                            }
                         }
-                        _ => Response::builder()
-                            .status(StatusCode::BAD_REQUEST)
-                            .header("Content-Type", "text/plain")
-                            .body(Body::from("wrong keytype"))
-                            .unwrap(),
+                        _ => (
+                            StatusCode::BAD_REQUEST,
+                            [(header::CONTENT_TYPE, "text/plain".to_string())],
+                            "wrong keytype".to_string(),
+                        ),
                     }
                 } else {
-                    Response::builder()
-                        .status(StatusCode::LOCKED)
-                        .header("Content-Type", "text/plain")
-                        .body(Body::from("database was locked"))
-                        .unwrap()
+                    (
+                        StatusCode::LOCKED,
+                        [(header::CONTENT_TYPE, "text/plain".to_string())],
+                        "database is locked".to_string(),
+                    )
                 }
             }
             "INSERT" => todo!(),
-            _ => Response::builder()
-                .status(StatusCode::BAD_REQUEST)
-                .header("Content-Type", "text/plain")
-                .body(Body::from("invalid operation"))
-                .unwrap(),
+            _ => (
+                StatusCode::BAD_REQUEST,
+                [(header::CONTENT_TYPE, "text/plain".to_string())],
+                "invalid operation".to_string(),
+            ),
         }
     } else {
-        Response::builder()
-            .status(StatusCode::BAD_REQUEST)
-            .header("Content-Type", "text/plain")
-            .body(Body::from("missing operation field"))
-            .unwrap()
+        (
+            StatusCode::BAD_REQUEST,
+            [(header::CONTENT_TYPE, "text/plain".to_string())],
+            "missing operation field".to_string(),
+        )
     }
 }
